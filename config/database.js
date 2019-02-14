eeac75b431aa7cb51be0bc25bfefb32e99acb98b
Max lima <maxwell.lima@cdts.fiocruz.br>
2019-02-14 16:19:20 -0200

    Ajustar conexão com banco

--- config/database.js
+++ config/database.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/base');
+mongoose.Promise = global.Promise;
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 module.exports = (mongoose);
