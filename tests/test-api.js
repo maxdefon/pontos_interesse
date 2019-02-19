6a7d6c7aff53a4003225d095fc662c02db48bc85
Max Lima <maxlima@MacBook-Pro-de-Max.local>
2019-02-18 22:51:15 -0300

    Adicionar configurações do banco aos testes

--- tests/test-api.js
+++ tests/test-api.js
@@ -1,21 +1,36 @@
+const db = require('../config/database');
+const app = require('../config/custom-express')();
 const chai = require('chai');
 const expect = chai.expect;
-const request = require('request');
 const chaihttp = require('chai-http');
-const baseUrl = "http://localhost:5000";
 chai.use(chaihttp);
 
-describe("Testando API", () => {
+before(done => {
+	db.connect().then(() => done()).catch(done);
+});
+
+after(done => {
+	db.close().then(() => done()).catch(done);
+});
+
+describe("Testando rota inicial da API", () => {
+
 
 	it('Api deve retornar status 200', done => {
-		request(baseUrl, (error, response, body) => {
+		chai.request(app)
+		.get('/')
+		.end((error, response) =>{
 			expect(response.statusCode).to.equal(200);
 			done();
 		});
+
 	});
 
+});
+
+describe("Testando endpoints dos pontos de interesse", () => {
 	it('Deve cadastrar um ponto de interesse', done => {
-		chai.request(baseUrl)
+		chai.request(app)
 		.post('/pois/cadastrar')
 		.send({
 			'_method': 'post',
@@ -33,7 +48,7 @@ describe("Testando API", () => {
 	});
 
 	it('Deve retornar uma lista de pontos de interesse', done => {
-  	chai.request(baseUrl)
+  	chai.request(app)
 		.get('/pois/listar')
 		.then(response =>{
 			expect(response.statusCode).to.equal(200);
@@ -43,18 +58,31 @@ describe("Testando API", () => {
 	});
 
 	it('Deve retornar uma lista de pontos de interesse por proximidade', done => {
-		chai.request(baseUrl)
-		.get('/pois/listar')
-		.query({
-			coordinateX: 20, 
-			coordinateY: 10,
-			maxDistance: 10,
-		})
+		let coordX = 20;
+		let coordY = 10;
+		let maxDistance = 10;
+		chai.request(app)
+		.get(`/pois/listar-coordernadas/${coordX}/${coordY}/${maxDistance}`)
 		.then(response =>{
 			expect(response.statusCode).to.equal(200);
 			expect(response.body.pois[0].name).to.equal('Lanchonete');
 			done();
-		});
+		})
+		.catch(done);
+	});
+
+	it('Deve retornar erro, distancia máxima não pode ser negativo', done => {
+		let coordX = 20;
+		let coordY = 10;
+		let maxDistance = -10;
+		chai.request(app)
+		.get(`/pois/listar-coordernadas/${coordX}/${coordY}/${maxDistance}`)
+		.then(response =>{
+			expect(response.statusCode).to.equal(200);
+			expect(response.body.error.errmsg).to.equal('$maxDistance must be non-negative');
+			done();
+		})
+		.catch(done);
 	});
 
 });
